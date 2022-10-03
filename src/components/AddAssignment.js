7adfbcf43023a0e20580fa31e0fbff66e8cad81e
Twilliams073 <tristanwilliamsrc@gmail.com>
2022-10-03 12:34:05 -0700

    working on Add Assignment

--- src/components/AddAssignment.js
+++ src/components/AddAssignment.js
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 import Cookies from 'js-cookie';
 import Button from '@mui/material/Button';
 import Radio from '@mui/material/Radio';
-import {DataGrid} from '@mui/x-data-grid';
+import TextField from '@mui/material/TextField';
 import {SERVER_URL} from '../constants.js'
 
 // NOTE:  for OAuth security, http request must have
 //   credentials: 'include' 
 //
 
-class Assignment extends React.Component {
+class AddAssignment extends React.Component {
     constructor(props) {
       super(props);
       this.state = {selected: 0, assignments: []};
@@ -21,72 +21,32 @@ class Assignment extends React.Component {
    componentDidMount() {
     this.fetchAssignments();
   }
- 
-  fetchAssignments = () => {
-    console.log("Assignment.fetchAssignments");
-    const token = Cookies.get('XSRF-TOKEN');
-    fetch(`${SERVER_URL}/gradebook`, 
-      {  
-        method: 'GET', 
-        headers: { 'X-XSRF-TOKEN': token }
-      } )
-    .then((response) => response.json()) 
-    .then((responseData) => { 
-      if (Array.isArray(responseData.assignments)) {
-        //  add to each assignment an "id"  This is required by DataGrid  "id" is the row index in the data grid table 
-        this.setState({ assignments: responseData.assignments.map((assignment, index) => ( { id: index, ...assignment } )) });
-      } else {
-        toast.error("Fetch failed.", {
-          position: toast.POSITION.BOTTOM_LEFT
-        });
-      }        
-    })
-    .catch(err => console.error(err)); 
+
+  handleSubmit = () => {
+    const { attempt, alias } = this.state;
+    this.props.submit( attempt.trim(), alias.trim() );
   }
-  
-   onRadioClick = (event) => {
-    console.log("Assignment.onRadioClick " + event.target.value);
-    this.setState({selected: event.target.value});
+ 
+  handleChange = (event) =>  {
+     this.setState({[event.target.name]: event.target.value});
   }
   
   render() {
-     const columns = [
-      {
-        field: 'assignmentName',
-        headerName: 'Assignment',
-        width: 400,
-        renderCell: (params) => (
-          <div>
-          <Radio
-            checked={params.row.id == this.state.selected}
-            onChange={this.onRadioClick}
-            value={params.row.id}
-            color="default"
-            size="small"
-          />
-          {params.value}
-          </div>
-        )
-      },
-      { field: 'courseTitle', headerName: 'Course', width: 300 },
-      { field: 'dueDate', headerName: 'Due Date', width: 200 }
-      ];
       
-      const assignmentSelected = this.state.assignments[this.state.selected];
-      return (
-          <div align="left" >
-            <h4>Assignment(s) ready to grade: </h4>
-              <div style={{ height: 450, width: '100%', align:"left"   }}>
-                <DataGrid rows={this.state.assignments} columns={columns} />
-              </div>                
-            <Button component={Link} to={{pathname:'/gradebook',   assignment: assignmentSelected }} 
-                    variant="outlined" color="primary" disabled={this.state.assignments.length===0}  style={{margin: 10}}>
-              Grade
-            </Button>
-            <ToastContainer autoClose={1500} /> 
-          </div>
-      )
+      <div align="left" >
+        <TextField autoFocus style = {{width:200}} label="Assignment Name" name="assignmentName" 
+             onChange={this.handleChange} value={this.state.attempt} />
+	<br/>
+	<TextField autoFocus style = {{width:200}} label="Due Date" name="dueDate" 
+             onChange={this.handleChange} value={this.state.attempt} />
+	<br/>
+	<TextField autoFocus style = {{width:200}} label="Course Name" name="courseName" 
+             onChange={this.handleChange} value={this.state.attempt} />
+	<br/>
+	<Button variant="outlined" color="primary" style={{margin: 10}}
+              onClick={this.handleSubmit} >Submit</Button>
+      </div>
   }
 }  
 
-export default Assignment;
+export default AddAssignment;
